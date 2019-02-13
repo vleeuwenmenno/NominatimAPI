8f424d0e154c5afe81b34d30185c6ce8e66d9d7e
Menno van Leeuwen <46482143+vleeuwenmenno@users.noreply.github.com>
2019-02-13 12:50:53 +0100

    Update address.dart
    Added new methods for use with lists of addresses.

--- lib/address.dart
+++ lib/address.dart
@@ -8,29 +8,54 @@ class Address
 		this.osm_type,
 
 		this.country,
-        this.countryIsoCode,
+        	this.countryIsoCode,
 
 		this.road,
 		this.housenumber,
 
 		this.city,
 		this.postcode,
-        this.suburb,
+        	this.suburb,
 		this.state
 	});
 
+    static Address findAddress(String query, List<Address> list)
+    {
+        for(Address a in list)
+        {
+            if (query == "${a.road} ${a.housenumber}, ${a.city}")
+                return a;
+        }
+
+        return null;
+    }
+
+    static List<String> toListString(String query, List<Address> list)
+    {
+        List<String> l = [];
+        for(Address a in list)
+        {
+            l.add("${a.road} ${a.housenumber}, ${a.city}");
+        }
+
+        return l;
+    }
+
+    /// Public vars
 	final LatLng coordinates;
 
 	final String osm_type;
 
 	final String country;
-    final String countryIsoCode;
+    	final String countryIsoCode;
 
 	final String road;
 	final String housenumber;
 
 	final String city;
 	final String postcode;
-    final String suburb;
+    	final String suburb;
 	final String state;
+
+    /// Public vars ^^^
 }
